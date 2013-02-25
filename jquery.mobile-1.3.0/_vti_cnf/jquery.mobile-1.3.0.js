56eee1612ea3872195c64e2d95ce30ac61b7148b
316karan <karan.kumar@letsintern.com>
2013-02-26 03:23:53 +0530

    ll

--- jquery.mobile-1.3.0/_vti_cnf/jquery.mobile-1.3.0.js
+++
@@ -1,9 +0,0 @@
-vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|22 Feb 2013 01:01:50 -0000
-vti_extenderversion:SR|12.0.0.0
-vti_author:SR|karan\\316karan
-vti_modifiedby:SR|karan\\316karan
-vti_timecreated:TR|22 Feb 2013 01:01:50 -0000
-vti_cacheddtm:TX|22 Feb 2013 01:01:50 -0000
-vti_filesize:IR|355848
-vti_backlinkinfo:VX|
